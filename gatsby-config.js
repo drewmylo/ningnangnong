b25b33c71eeb12b3963ca9336f2902c002da91df
drewmylo <andrewmylonas@live.com>
2024-10-03 21:02:57 +1000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ningnangnong`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
